3c9271fd018d3cd1b3f3ac4a37a5251dd0c36024
Felix Hermann <17felix2000@gmail.com>
2021-01-11 16:07:39 +0100

    First commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Starting seeds.."
+
+puts "Creating flats"
+Ashtray = Flat.create(
+  address: "888 Brannan St, San Francisco, CA 94103, United States",)
